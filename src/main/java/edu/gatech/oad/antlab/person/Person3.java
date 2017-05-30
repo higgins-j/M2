9cab327df7613f5336cd73a65f7fc7efb5d45a25
JackAbrams <jackabrams96@gmail.com>
2017-05-30 16:02:03 -0400

    Corrected a minor error in person3.java

--- src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -43,7 +43,7 @@ public class Person3 {
 	 */
 	private String calc(String input) {
 	    String newString = "";
-	    for (int i = input.length() - 1; i > 0; i--) {
+	    for (int i = input.length() - 1; i >= 0; i--) {
 	        newString = newString + input.charAt(i);
 	  }
 	  return newString;
